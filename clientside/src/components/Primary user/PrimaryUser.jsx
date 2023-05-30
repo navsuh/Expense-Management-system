8cdede87a9335394c70143ef824dcd3c7936c7aa
SuryakantLad <suryakantl@valueaddsofttech.com>
2023-05-30 17:43:12 +0530

    adding style

--- clientside/src/components/Primary user/PrimaryUser.jsx
+++ clientside/src/components/Primary user/PrimaryUser.jsx
@@ -34,12 +34,10 @@ const PrimaryUser = () => {
       <div className="p-4 sm:ml-64">
         
         <Sidebar />
-        <h1 className="ml-7 font-medium">Dashboard</h1>
+        <h1 className="ml-7 font-semibold text-2xl">Dashboard</h1>
         <Tablist tabList={tabList} />
-       
-        {/* <ExpenseTypeTable expenseList={expenseList}/> */}
         <Outlet />
-        {/* <UserTable userList={userList}/> */}
+      
       </div>
     </>
   );
