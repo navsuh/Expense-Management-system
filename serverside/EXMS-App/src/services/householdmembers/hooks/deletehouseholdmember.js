14ad0ba2b237f23bc5d48c2fe412ff9db234330e
Naveed Suhail <naveeds@valueaddsofttech.com>
2023-06-13 17:56:59 +0530

    Update deletehouseholdmember.js

--- serverside/EXMS-App/src/services/householdmembers/hooks/deletehouseholdmember.js
+++ serverside/EXMS-App/src/services/householdmembers/hooks/deletehouseholdmember.js
@@ -2,11 +2,17 @@ import { emailhandler } from '../../../helpers/emailhandler.js'
 export const deletehouseholdmember =()=>{
     return async(context)=>{
         console.log(context.id);
+        
         const householdMemberService=context.app.service("householdmembers")
         const findhouseholdMember= await householdMemberService.find( { query: { _id:context.id } });
         console.log(findhouseholdMember);
         const userService=context.app.service("users")
-        const finduser = await userService.remove( findhouseholdMember.data[0].user);
+        const finduser=await userService.find( { query: { _id:findhouseholdMember.data[0].user} });
+        console.log(finduser);
+        if(finduser.data[0].role==='member'){
+          const deleteuser = await userService.remove( findhouseholdMember.data[0].user);
+        }
+        
 
       
     
