01a56d3dbcc6d651e674fc6fbcf4f144fc9709f4
Naveed Suhail <naveeds@valueaddsofttech.com>
2023-06-13 18:35:52 +0530

    Update houseHoldTable.jsx

--- clientside/src/components/Primary user/houseHoldTable.jsx
+++ clientside/src/components/Primary user/houseHoldTable.jsx
@@ -47,7 +47,7 @@ const HouseholdTable = (props) => {
             </tr>
           </thead>
           <tbody>
-            {houseHoldList.map((eachHousehold) => (
+            {houseHoldList.filter((h)=>h.name.toLowerCase().includes(searchQuery.toLowerCase())||h.name.toLowerCase().includes(searchQuery.toLowerCase())).map((eachHousehold) => (
               <tr className="border-b bg-gray-50 " key={eachHousehold._id}>
                 <td className="px-6 py-4">{eachHousehold.name}</td>
                 <td className="px-6 py-4">
